96f8875e0c257eb84c60399e2eb676cf73788db6
Sev7en <464700301@qq.com>
2018-05-10 14:51:20 +0800

    icon\layer-mask

--- examples/test/IconTest.vue
+++ examples/test/IconTest.vue
@@ -7,19 +7,6 @@
       </span>
       <div :key="name" v-if="(idx + 1)%10 == 0"></div>
     </template>
-
-    <we-icon name="loading-annual-ring" :loading="true"></we-icon>
-    <we-icon name="loading-black-hole" :loading="true"></we-icon>
-    <we-icon name="loading-drop" :loading="true"></we-icon>
-    <we-icon name="loading-drop-circle" :loading="true"></we-icon>
-    <we-icon name="loading-fading" :loading="true"></we-icon>
-    <we-icon name="loading-fading-full" :loading="true"></we-icon>
-
-    <we-icon name="loading-fading-point" :loading="true"></we-icon>
-    <we-icon name="loading-point-line" :loading="true"></we-icon>
-    <we-icon name="loading-refresh" :loading="true"></we-icon>
-    <we-icon name="loading-snakes" :loading="true"></we-icon>
-
   </div>
 </template>
 
