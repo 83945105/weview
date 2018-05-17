061a68757acb9bf0113d8c4d7fb7274d82d6f73e
wx_83945105 <18606167809@163.com>
2018-05-17 15:57:52 +0800

    项目构建中

--- examples/test/LayerTest.vue
+++ examples/test/LayerTest.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    <we-layer></we-layer>
+    <we-layer :height="500">
+      <div style="width: 50px;height: 600px;">+4566+46464646</div>
+      <!--<iframe frameborder="0" name="layerFrame" scrolling="auto" width="100%" :height="frameHeight" src="http://wwww.baidu.com"></iframe>-->
+
+
+      <template slot="footer">
+        <we-button>确定</we-button>
+        <we-button>确定</we-button>
+        <we-button>确定</we-button>
+      </template>
+
+    </we-layer>
   </div>
 </template>
 
--- packages/layer/src/Layer.vue
+++ packages/layer/src/Layer.vue
@@ -1,21 +1,38 @@
 <template>
   <div>
-    <div class="we-layer-mask-bg"></div>
-    <div class="we-layer-prompt is-position-auto" style="width: 100%;height: 100%">
-      <div class="we-layer-prompt-top move layer-prompt-top">
-        <div class="we-layer-prompt-top-title">信息提示</div>
-        <div class="we-layer-prompt-top-close">
-          <we-icon name="close"></we-icon>
-        </div>
+    <div :class="[maskBgClass]"></div>
+    <div class="is-position-auto"
+         :class="[layerClass]"
+         :style="style"
+    >
+      <div v-if="showHeader"
+           class="move"
+           :class="[headerClass]"
+           @mousedown="mousedown"
+           @mouseup="mouseup"
+           @mousemove="mousemove"
+      >
+        <slot name="header">
+          <div :class="[titleClass]">{{title}}</div>
+          <div class="we-layer-prompt-top-close">
+            <icon name="close"></icon>
+          </div>
+        </slot>
       </div>
-      <div class="we-layer-prompt-content"
-           style="width: 100%;height: 80%;background-color: #3db670;border: 1px solid red">
-        <div style="width: 500px;height: 100px;">+4566+46464646</div>
-        <!--<iframe frameborder="0" name="layerFrame" scrolling="auto" width="100%" :height="frameHeight" src="http://wwww.baidu.com"></iframe>-->
+      <div :class="[contentClass]"
+           style="background-color: #3db670;border: 1px solid red;"
+           :style="{
+            height: this._height ? `${this._height - headerHeight - footerHeight - 1}px` : undefined
+           }"
+      >
+        <slot></slot>
       </div>
-      <div class="we-layer-prompt-bottom layer-prompt-bottom">
-        <we-button type="primary">确定</we-button>
-        <we-button>取消</we-button>
+      <div v-if="showFooter"
+           :class="[footClass, footAlignClass]">
+        <slot name="footer">
+          <we-button type="primary">确定</we-button>
+          <we-button>取消</we-button>
+        </slot>
       </div>
     </div>
   </div>
@@ -23,13 +40,14 @@
 
 <script>
 
-  import Conf from '../../../src/mixins/conf';
+  import Conf from '../../../src/mixins/conf.js';
 
-  import WeButton from '../../button/src/Button';
+  import WeButton from '../../button/src/Button.vue';
+  import WeIcon from '../../icon/src/Icon.vue';
 
   export default {
 
-    components: {WeButton},
+    components: {WeButton: WeButton, Icon: WeIcon},
 
     name: `${Conf.prefixCls}-layer`,
 
@@ -37,10 +55,17 @@
 
     optionName: `layer`,
 
+    mixins: [Conf],
+
     data() {
       return {
         widthUnit: 'px',
-        heightUnit: 'px'
+        heightUnit: 'px',
+        isDrag: false,
+        left: '50%',
+        top: '50%',
+        headerDom: undefined,
+        footerDom: undefined
       };
     },
 
@@ -50,19 +75,127 @@
         default: 300
       },
       height: {
-        type: [Number, String],
-        default: 200
+        type: [Number, String]
+      },
+      showHeader: {
+        type: Boolean,
+        default: true
+      },
+      showFooter: {
+        type: Boolean,
+        default: true
+      },
+      title: {
+        type: String,
+        default: '提示'
+      },
+      footerAlign: {//left center right
+        type: String,
+        default: 'right'
       }
     },
 
     computed: {
       _width() {
-
+        return this.width ? parseInt(`${this.width}`.replace(/[^0-9]/g, "")) : undefined;
       },
-      contentWidth() {
+      _height() {
+        return this.height ? parseInt(`${this.height}`.replace(/[^0-9]/g, "")) : undefined;
+      },
+      style() {
+        return {
+          width: this._width ? this._width > 100 ? `${this._width}px` : `${this._width}%` : undefined,
+          height: this._height ? this._height > 100 ? `${this._height}px` : `${this._height}%` : undefined,
+          left: this.left,
+          top: this.top
+        };
+      },
+      headerHeight() {
+        return (this.showHeader && this.headerDom) ? this.headerDom.clientHeight : 0;
+      },
+      footerHeight() {
+        return (this.showFooter && this.footerDom) ? this.footerDom.clientHeight : 0;
+      },
+      maskBgClass() {
+        return `${this.prefixCls}-layer-mask-bg`;
+      },
+      layerClass() {
+        return `${this.prefixCls}-layer-prompt`;
+      },
+      headerClass() {
+        return `${this.prefixCls}-layer-prompt-top`;
+      },
+      titleClass() {
+        return `${this.prefixCls}-layer-prompt-top-title`;
+      },
+      contentClass() {
+        return `${this.prefixCls}-layer-prompt-content`;
+      },
+      footClass() {
+        return `${this.prefixCls}-layer-prompt-bottom`;
+      },
+      footAlignClass() {
+        switch (this.footerAlign) {
+          case 'left':
+            return 'tal';
+          case 'center':
+            return 'tac';
+          default:
+            return undefined;
+        }
       }
-    }
+    },
 
+    watch: {
+      width() {
+        this.initWidthUnit();
+      },
+      height() {
+        this.initHeightUnit();
+      }
+    },
+
+    methods: {
+      initWidthUnit() {
+        if (typeof this.width === 'string') {
+          this.widthUnit = this.width.replace(/[0-9]/g, "");
+        }
+      },
+      initHeightUnit() {
+        if (typeof this.height === 'string') {
+          this.heightUnit = this.height.replace(/[0-9]/g, "");
+        }
+      },
+      getWindowWidth() {
+        return window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+      },
+      getWindowHeight() {
+        return window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+      },
+      mousedown(e) {
+        this.isDrag = true;
+      },
+      mouseup(e) {
+        this.isDrag = false;
+      },
+      mousemove(e) {
+        if (this.isDrag) {
+
+        }
+
+      }
+    },
+
+    created() {
+      this.initWidthUnit();
+      this.initHeightUnit();
+    },
+
+    mounted() {
+      this.headerDom = this.$el.querySelector(`.${this.headerClass}`);
+      this.footerDom = this.$el.querySelector(`.${this.footClass}`);
+      console.log(this.headerHeight)
+    }
 
   }
 </script>
