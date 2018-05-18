7bc18ffa0a254cd5e70f3f410b39042d5abe08fc
wx_83945105 <18606167809@163.com>
2018-05-18 16:52:18 +0800

    项目构建中

---
+++ packages/animation/src/Animation.vue
@@ -0,0 +1,24 @@
+<template>
+  <transition>
+    <slot></slot>
+  </transition>
+</template>
+
+<script>
+
+  import Conf from '../../../src/mixins/conf.js';
+
+  export default {
+
+    name: `${Conf.prefixCls}-animation`,
+
+    componentName: `${Conf.prefixNameCls}Animation`,
+
+    props: {
+      name: {
+        type: String,
+        default: 'fade'
+      }
+    }
+  }
+</script>
