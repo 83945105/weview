bebbb1422738da51e813886fdcb2535f2644e0f6
Sev7en <464700301@qq.com>
2018-09-30 17:30:09 +0800

    页面提示页面

--- examples/test/ScrollBarTest.vue
+++ examples/test/ScrollBarTest.vue
@@ -69,6 +69,62 @@
         </div>
       </div>
     </div>
+
+
+    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+      <el-menu-item index="1">处理中心</el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">我的工作台</template>
+        <el-menu-item index="2-1">选项1</el-menu-item>
+        <el-menu-item index="2-2">选项2</el-menu-item>
+        <el-menu-item index="2-3">选项3</el-menu-item>
+        <el-submenu index="2-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="2-4-1">选项1</el-menu-item>
+          <el-menu-item index="2-4-2">选项2</el-menu-item>
+          <el-menu-item index="2-4-3">选项3</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="3" disabled>消息中心</el-menu-item>
+      <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+    </el-menu>
+    <div class="line"></div>
+    <el-menu
+      :default-active="activeIndex2"
+      class="el-menu-demo"
+      mode="horizontal"
+      @select="handleSelect"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+      <el-menu-item index="1">处理中心</el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">我的工作台</template>
+        <el-menu-item index="2-1">选项1</el-menu-item>
+        <el-menu-item index="2-2">选项2</el-menu-item>
+        <el-menu-item index="2-3">选项3</el-menu-item>
+        <el-submenu index="2-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="2-4-1">选项1</el-menu-item>
+          <el-menu-item index="2-4-2">选项2</el-menu-item>
+          <el-menu-item index="2-4-3">选项3</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="3" disabled>消息中心</el-menu-item>
+      <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+      <el-submenu index="5" :popper-append-to-body="false">
+        <template slot="title">我的工作台</template>
+        <el-menu-item index="5-1">选项1</el-menu-item>
+        <el-menu-item index="5-2">选项2</el-menu-item>
+        <el-menu-item index="5-3">选项3</el-menu-item>
+        <el-submenu index="5-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="5-4-1">选项1</el-menu-item>
+          <el-menu-item index="5-4-2">选项2</el-menu-item>
+          <el-menu-item index="5-4-3">选项3</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+    </el-menu>
   </div>
 </template>
 
@@ -90,7 +146,9 @@
         verticalTranslate: 0,
         horizontalWidth: 0,
         horizontalTranslate: 0,
-        scrollRatio: 0
+        scrollRatio: 0,
+        activeIndex: '1',
+        activeIndex2: '1'
       }
     },
 
@@ -111,6 +169,9 @@
     },
 
     methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      },
       updateScrollBar(){
         let inner = document.getElementById("scrollBar");
         let heightPercentage = inner.clientHeight * 100 / inner.scrollHeight;
---
+++ examples/test/pagePromptsTest.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div class="we-page-prompts">
+      <img src="../../static/src/images/page_prompts/no_data_img.png" />
+      <div class="we-page-prompts-text">
+        抱歉暂无信息。
+      </div>
+      <div class="we-page-prompts-button">
+        <we-icon>重新加载</we-icon>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import WeIcon from "../../packages/icon/src/Icon.vue";
+  import WeButton from "../../packages/button/src/Button.vue";
+
+  export default {
+    name: "page-prompts-test",
+
+    components: {
+      weIcon: WeIcon,
+      weButton: WeButton
+    },
+    data() {
+      return {
+
+      }
+    },
+
+    computed: {
+    },
+
+    created(){
+
+    },
+
+    methods: {
+
+    },
+
+    mounted(){
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
